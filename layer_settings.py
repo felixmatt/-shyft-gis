d915f8728dca82e754272b7b165b846f6f4d8501
felixmatt <f.n.matt@geo.uio.no>
2018-06-04 14:03:52 +0200

    attempting to make scripts more userfriendly

--- layer_settings.py
+++ layer_settings.py
@@ -4,14 +4,14 @@ import os
 
 # local imports
 HOME= os.environ['HOME']
-workdir = HOME+"/Documents/github/shyft-gis"
+workdir = HOME+"/Documents/github/shyft-gis/project"
 sys.path.append(workdir)
 import process_layers as proc
 
 def setup_finse(DATA_PATH):
-    ptp = DATA_PATH+'shyft_catchments/' # path to polygon
-    ptr = DATA_PATH+'shyft_catchments/' # path to raster
-    ptd = DATA_PATH+'dem/'
+    ptp = DATA_PATH # path to polygon
+    ptr = DATA_PATH+'/corine_land_cover_2012/' # path to raster
+    ptd = DATA_PATH+'/dem/'
     plf = ["12.166_shyft_gridded/12.166_gridded.shp", 
                             "50.2_shyft_gridded/50.2_gridded.shp", 
                             "50.5_shyft_gridded/50.5_gridded.shp", 
@@ -25,18 +25,18 @@ def setup_finse(DATA_PATH):
 
     pcn = ["12.166_centroids", "50.2_centroids", "50.5_centroids", "50.8_centroids", "50.10_centroids", "50.11_centroids", "50.13_centroids", "50.38_centroids"]
 
-    rlf = ["g100_clc12_V18_5a/corine_forest_fraction.tif", 
-            "g100_clc12_V18_5a/corine_lake_fraction.tif",
-            "g100_clc12_V18_5a/corine_glacier_fraction.tif",
-            "g100_clc12_V18_5a/corine_reservoir_fraction.tif"] # raster layer files
+    rlf = ["/forest_fraction.tif", 
+            "/lake_fraction.tif",
+            "/glacier_fraction.tif",
+            "/reservoir_fraction.tif"] # raster layer files
     dff = ["dem_finse.tif", "slope_finse.tif", "aspect_finse.tif"] # dem feature files
     finse = proc.catchment_layers("finse", ptp, ptd, ptr, plf, pcn, rlf, dff)
     return finse
 
 
-DATA_PATH= HOME+'/Documents/finse/GIS_data/'
-DATA_INT=DATA_PATH+'shyft_catchments/int'
-OUT = DATA_PATH+'/shyft_catchments/cell_data.nc'
+DATA_PATH= HOME+'/Documents/github/shyft-gis/project/'
+DATA_INT=DATA_PATH+'/Documents/github/shyft-gis/project/int'
+OUT = DATA_PATH+'/Documents/github/shyft-gis/project/cell_data.nc'
 
 finse = setup_finse(DATA_PATH)
 finse.copy_files(DATA_INT) # create intermediate files and point them to polygon_layer_files
